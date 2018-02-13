59c8851edbee2b108078f44ad49b95a2f4d1af22
PrinceNathaniel <nathanielxuyc@gmail.com>
2018-02-13 17:38:46 -0500

    0213-01

--- imdb/imdb/spiders/imdb_spider.py
+++ imdb/imdb/spiders/imdb_spider.py
@@ -5,11 +5,8 @@ class imdbSpider(Spider):
     name='imdb_spider'
     allowed_urls = ['http://www.imdb.com']
     #start_urls=['http://www.imdb.com/title/tt0120891/reviews?ref_=tt_ov_rt']
-    for i in range(0,20):
-        year1=1998+i
-        year2=1999+i
-        start_urls=['http://www.imdb.com/search/title?release_date='+str(year1)+'-01-01,'+str(year2)+'-01-01&page=%s&ref_=adv_nxt'% page for page in range(0,10)]
-    #start_urls=['http://www.imdb.com/search/title?release_date=1999-01-01,2000-01-01']
+    start_urls=['http://www.imdb.com/search/title?release_date=2017-01-01,2018-01-01&page=%s&ref_=adv_nxt'% page for page in range(0,3)]
+    #start_urls=['http://www.imdb.com/search/title?release_date=2000-01-01,2001-01-01']
     def parse(self,response):
         rows=response.xpath('//h3[@class="lister-item-header"]')
         for row in rows:
